5e0275440bc6286fc084fe8d262819516b2582c4
yumin00 <imeumine8869@naver.com>
2025-01-07 23:20:57 +0900

    impl: study final in java

---
+++ src/final/final1/FinalLocalMain.java
@@ -0,0 +1,14 @@
+package final1;
+
+/*
+final 키워드는 말 그대로 끝! 이라는 뜻이다.
+변수에 final이라는 키워드가 붙으면 더이상 변수의 값을 변경할 수 없다는 의미이다.
+ */
+
+public class FinalLocalMain {
+    public static void main() {
+        final int data1;
+        data1 = 10; // 이렇게 변수를
+
+    }
+}
