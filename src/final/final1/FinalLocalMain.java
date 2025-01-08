f66420a2d6ba914f1af8dac53121fea8190524b1
yumin00 <imeumine8869@naver.com>
2025-01-08 16:26:17 +0900

    impl: study final in java

--- src/final/final1/FinalLocalMain.java
+++ src/final/final1/FinalLocalMain.java
@@ -8,7 +8,18 @@ final 키워드는 말 그대로 끝! 이라는 뜻이다.
 public class FinalLocalMain {
     public static void main() {
         final int data1;
-        data1 = 10; // 이렇게 변수를
+        data1 = 10; // 변수를 한 번만 할당할 수 있다.
 
+//        data1 = 30; // 컴파일 오류 발생
+        method(data1);
+    }
+
+    /*
+    매개변수에 final이 붙으면 메서드 내부에서 매개변수의 값을 변경할 수 없다.
+    메서드 호출 시점에 사용된 값이 끝까지 사용된다!
+     */
+    static void method(final int parameter) {
+//        parameter = 20; 컴파일 오류 발생
+        System.out.println(parameter);
     }
 }
